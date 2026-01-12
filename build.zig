ad58880fa7baebe358ab1e4d2d0ffba8d8418545
Patryk Sondej <patryk.sondej@gmail.com>
2026-01-12 21:12:10 +0100

    refactor tests

--- build.zig
+++ build.zig
@@ -4,17 +4,23 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const mod = b.addModule("rfid_codec", .{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    // const mod = b.addModule("rfid_codec", .{
+    //     .root_source_file = b.path("src/root.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
 
-    const mod_tests = b.addTest(.{
-        .root_module = mod,
-    });
-    const run_mod_tests = b.addRunArtifact(mod_tests);
+    {
+        const tests = b.addTest(.{
+            .root_module = b.createModule(.{
+                .target = target,
+                .optimize = optimize,
+                .root_source_file = b.path("src/root.zig"),
+            }),
+        });
 
-    const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_mod_tests.step);
+        const run_tests = b.addRunArtifact(tests);
+        const test_step = b.step("test", "Run unit tests");
+        test_step.dependOn(&run_tests.step);
+    }
 }
--- src/root.zig
+++ src/root.zig
@@ -1,16 +1,10 @@
+const std = @import("std");
 pub const decoders = @import("decoders.zig");
 
-// pub const manchester = decoder.Decoder(0, .Manchester);
-// pub const anticollision = decoder.Decoder(0, .AntiCollision);
 pub const bplc = @import("bplc.zig");
-
 pub const hitags = @import("hitags.zig");
 pub const em4100 = @import("em4100.zig");
 
-// run tests
-comptime {
-    _ = bplc;
-    _ = hitags;
-    _ = em4100;
-    _ = decoders;
+test {
+    std.testing.refAllDecls(@This());
 }
